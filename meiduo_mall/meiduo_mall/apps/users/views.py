b6a273f245059ee61ba83a0c247cc57dd924c9b9
王经理 <managewang@163.com>
2019-06-09 15:29:23 +0800

    用户注册接口定义

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -4,12 +4,33 @@ from django.shortcuts import render
 from django.views import View
 
 
+# class RegisterView(View):
+#     """用户注册"""
+#     def get(self,request):
+#         """
+#         提供注册界面
+#         :param request:请求对象
+#         :return: 注册界面
+#         """
+#         return render(request,'register.html')
+
+# 注册视图
 class RegisterView(View):
     """用户注册"""
-    def get(self,request):
+
+    def get(self, request):
         """
         提供注册界面
-        :param request:请求对象
+        :param request: 请求对象
         :return: 注册界面
         """
-        return render(request,'register.html')
+        return render(request, 'register.html')
+
+    def post(self, request):
+        """
+        实现用户注册
+        :param request: 请求对象
+        :return: 注册结果
+        """
+        pass
+
